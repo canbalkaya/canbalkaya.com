f1b1fc776e8d5e9652760cec53c039d40f08ea99
Can Balkaya <canbalkya@gmail.com>
2021-03-13 21:17:25 +0300

    Footer is currently finished.

--- my-website/src/components/Footer.js
+++ my-website/src/components/Footer.js
@@ -6,10 +6,13 @@ import Col from "react-bootstrap/Col";
 function Footer() {
     return(
         <footer className = "mt-5">
-            <Container>
-                <Row>
+            <Container fluid = {true }>
+                <Row className = "border-top justify-content-between p-3">
                     <Col className = "p-0" md = {3} sm = {12}>
                         Can Balkaya
+                    </Col>
+                    <Col className = "">
+                    
                     </Col>
                 </Row>
             </Container>
