809d187a569d46be7ca1666f61a9d522e79efcea
Can Balkaya <canbalkya@gmail.com>
2021-03-13 18:54:37 +0300

    Navbar is developed.

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, /* Route, Link */ } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
-import './App.css';
 import NavbarBrand from "react-bootstrap/esm/NavbarBrand";
+import Nav from "react-bootstrap/esm/Nav";
+import './App.css';
 
 class App extends React.Component {
   constructor(props) {
@@ -37,8 +38,16 @@ class App extends React.Component {
     return(
       <Router>
         <Container className = "p-0" fluid = { true }>
-          <Navbar> 
+          <Navbar className = "border-bottom"> 
             <Navbar.Brand>Can Balkaya</Navbar.Brand>
+            <Navbar.Toggle aria-controls = "navbar-toggle"/>
+            <Navbar.Collapse id = "navbar-toggle">
+              <Nav>
+                <Link className = "nav-link" to = "/">Home</Link>
+                <Link className = "nav-link" to = "/blog">Blog</Link>
+                <Link className = "nav-link" to = "/contact">Contact</Link>
+              </Nav>
+            </Navbar.Collapse>
           </Navbar>
         </Container>
       </Router>
