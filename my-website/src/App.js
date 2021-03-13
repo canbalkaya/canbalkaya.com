10c64f7e6dbd34b7223a774adb82a0a6390ebf06
Can Balkaya <canbalkya@gmail.com>
2021-03-13 18:56:07 +0300

    Nav is updated.

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import NavbarBrand from "react-bootstrap/esm/NavbarBrand";
@@ -42,7 +42,7 @@ class App extends React.Component {
             <Navbar.Brand>Can Balkaya</Navbar.Brand>
             <Navbar.Toggle aria-controls = "navbar-toggle"/>
             <Navbar.Collapse id = "navbar-toggle">
-              <Nav>
+              <Nav className = "ml-auto">
                 <Link className = "nav-link" to = "/">Home</Link>
                 <Link className = "nav-link" to = "/blog">Blog</Link>
                 <Link className = "nav-link" to = "/contact">Contact</Link>
