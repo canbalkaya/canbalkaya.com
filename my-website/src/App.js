9822b8108e159ebebb9a8a91c309ae8dcd5e62dc
Can Balkaya <canbalkya@gmail.com>
2021-03-13 18:48:42 +0300

    Navbar is added.

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, /* Route, Link */ } from "react-router-dom";
 import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
 import './App.css';
+import NavbarBrand from "react-bootstrap/esm/NavbarBrand";
 
 class App extends React.Component {
   constructor(props) {
@@ -34,8 +36,10 @@ class App extends React.Component {
   render() {
     return(
       <Router>
-        <Container fluid = { true }>
-          <p>Hellow, World!</p>
+        <Container className = "p-0" fluid = { true }>
+          <Navbar> 
+            <Navbar.Brand>Can Balkaya</Navbar.Brand>
+          </Navbar>
         </Container>
       </Router>
     );
